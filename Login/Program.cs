8c6890e840fa1796127f52d49f149e17b572348e
Perry Chen <2558544545qwerty@gmail.com>
2024-07-10 22:39:44 +0000

    Initial commit

--- Login/Program.cs
+++ Login/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading;
 
 namespace Login
 {
     internal class Program
     {
-        public enum VerifyStatus
+        /**
+         * Class for storing the information needed to create a menu option
+         */
+        public class Option
         {
-            Success,
-            UserNotFound,
-            WrongPassword,
+            public string Name { get; }
+            public Action Action { get; }
+            public Option(string name, Action action)
+            {
+                Name = name;
+                Action = action;
+            }
         }
 
         /**
@@ -21,8 +29,81 @@ namespace Login
         {
             public string Name { get; set; }
             public string Password { get; set; }
+
         }
 
+        /** 
+         * Hashes the raw text of the password entered into a hashed copy
+         */
+        private static string HashPassword(string password)
+        {
+            //STEP 1 Create the salt value with a cryptographic PRNG:
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            //STEP 2 Create the Rfc2898DeriveBytes and get the hash value:
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            //STEP 3 Combine the salt and password bytes for later use:
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            //STEP 4 Turn the combined salt+hash into a string for storage
+            string hashedPassword = Convert.ToBase64String(hashBytes);
+
+            return hashedPassword;
+        }
+
+
+        enum VerifyStatus
+        {
+            Success,
+            UserNotFound,
+            WrongPassword,
+        }
+
+        /**
+         * VerifyPassword verifys the user
+         * by checking the password against the one stored inside infoStore 
+         * matching the user name entered. 
+         * 
+         * @return: an Enum type which indicates the verification status 
+         * 
+         */
+
+        static VerifyStatus VerifyPassword(List<UserInfo> infoStore, string nameEntered, string passwordEntered)
+        {
+            //STEP 5 Verify the user - entered password against a stored password
+            /* Fetch the stored value */
+            UserInfo searched = infoStore.Find(info => info.Name == nameEntered);
+
+            if (searched == null)
+            {
+                return VerifyStatus.UserNotFound;
+            }
+
+            string savedPasswordHash = searched.Password;
+
+            /* Extract the bytes */
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+            /* Get the salt */
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            /* Compute the hash on the password the user entered */
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordEntered, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            /* Compare the results */
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    return VerifyStatus.WrongPassword;
+                }
+            }
+            return VerifyStatus.Success;
+        }
         /**
         * Asks the user for info which can be used
         * for authentication later during user login
@@ -47,35 +128,86 @@ namespace Login
                 return false;
             }
 
-            var newUser = new UserInfo { Name = userName, Password = password };
+            var hashedPassword = HashPassword(password);
+
+            var newUser = new UserInfo { Name = userName, Password = hashedPassword };
             // Memorize this
             infoStore.Add(newUser);
             return true;
         }
 
-        /**
-        * VerifyPassword verifys the user
-        * by checking the password against the one stored inside infoStore 
-        * matching the user name entered. 
-        * 
-        * @return: an Enum type which indicates the verification status 
-        * 
-        */
-        static VerifyStatus VerifyPassword(List<UserInfo> infoStore, string nameEntered, string passwordEntered)
+        public static List<Option> options = new List<Option>
+            {
+                new Option("Sign Up", () =>  EnterSignupProcedure()),
+                new Option("Login", () => EnterLoginProcedure()),
+                new Option("Exit", () => Environment.Exit(0)),
+            };
+
+        public static List<UserInfo> infoStore = new List<UserInfo>();
+
+        static void Main(string[] args)
         {
-
-            UserInfo searched = infoStore.Find(info => info.Name == nameEntered);
-
-            if (searched == null)
+            // Initialise variables for use
+            int indexSelected = 0;
+            bool running = true;
+            ConsoleKeyInfo keyInfo;
+            while (running)
             {
-                return VerifyStatus.UserNotFound;
-            }
-            if (searched.Password != passwordEntered)
-            {
-                return VerifyStatus.WrongPassword;
-            }
+                // For each loop, clear the terminal output and draw a new version of the menu
+                Console.Clear();
+                DrawMenu(options, indexSelected);
 
-            return VerifyStatus.Success;
+                // Read keyboard info to determine action
+                keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    // Update the menu option selected based on the index
+                    case ConsoleKey.UpArrow:
+                        if (indexSelected - 1 != -1)
+                        {
+                            indexSelected--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        // Only modify the index if it has not gone out of range
+                        if (indexSelected + 1 != options.Count)
+                        {
+                            indexSelected++;
+                        }
+                        break;
+                    // Quit using Q
+                    case ConsoleKey.Q:
+                        running = false;
+                        break;
+                    // Triggers a secondary procedure based on the option selected
+                    case ConsoleKey.Enter:
+                        options[indexSelected].Action.Invoke();
+                        break;
+                }
+            }
+        }
+        /**
+         * This function draws the menu on the terminal 
+         * @params options: 
+         *     Supplies the info needed to create each menu item
+         
+         * @params indexSelected:
+         *     Adds a ">" indicator to option[indexSelected]
+         */
+        private static void DrawMenu(List<Option> options, int indexSelected)
+        {
+            for (int i = 0; i < options.Count; i++)
+            {
+                var option = options[i];
+                if (i == indexSelected)
+                {
+                    Console.WriteLine($"> {option.Name}");
+                }
+                else
+                {
+                    Console.WriteLine(option.Name);
+                }
+            }
         }
 
         private static void EnterLoginProcedure()
@@ -115,52 +247,5 @@ namespace Login
             Console.WriteLine(welcomeMessage);
             LogUserInfo(infoStore);
         }
-
-        private static void DisplayMainMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Hi user! Welcome come to my simple login system");
-            Console.WriteLine("Please proceed by one of the following numbers:");
-
-            Console.WriteLine("");
-            for (int i = 0; i < menuItems.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}: {menuItems[i]}");
-            }
-            Console.WriteLine("");
-
-            Console.Write("The number you choose is:");
-        }
-
-        public static List<UserInfo> infoStore = new List<UserInfo>();
-        public static List<string> menuItems = new List<string> { "Sign up", "Log in", "exit" };
-        static void Main(string[] args)
-        {
-
-            bool running = true;
-            while (running)
-            {
-
-                DisplayMainMenu();
-
-                string optionSelected = Console.ReadLine();
-
-                switch (optionSelected)
-                {
-                    case "1":
-                        EnterSignupProcedure();
-                        break;
-                    case "2":
-                        EnterLoginProcedure();
-                        break;
-                    case "3":
-                        running = false;
-                        break;
-                }
-
-            }
-
-        }
     }
 }
-
